8ccdcf4e43af20043e6499c155657a1c7ce33c42
Oleg Kirichenko <olegkirichenko@Olegs-Mac-mini.local>
2018-03-29 09:52:14 -0400

    http-fetcher -> httpFetcher

--- test/promise.js
+++ test/promise.js
@@ -1,6 +1,5 @@
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
-const rio = require('riojs')
 
 const fromFile = require('../helpers/file-fetcher.js')
 const fci = require('../helpers/fetch-compile-install.js')
