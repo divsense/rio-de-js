a2c28a5d70f36d65ef668059743f8d8bb5231ac7
Oleg Kirichenko <olegkirichenko@Olegs-Mac-mini.local>
2018-04-01 14:30:40 -0400

    log added

--- libs/io.js
+++ libs/io.js
@@ -1,8 +1,9 @@
 const delay = time => new Promise(resolve => setTimeout(() => resolve(time), time))
 const delayBy = time => x => new Promise(resolve => setTimeout(() => resolve(x), time))
+const log = x => y => { console.log(x + ":" + JSON.stringify(y, null, 2)); return y }
 
 module.exports = {
-    names:[ 'delay', 'delayBy' ],
-    functions:[ delay, delayBy ]
+    names:[ 'delay', 'delayBy', 'log' ],
+    functions:[ delay, delayBy, log ]
 };
 
